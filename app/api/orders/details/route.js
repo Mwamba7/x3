b53947c7ec1845090a7855f46cde3cb296dd3b4f
Mwamba7 <jaddan87@gmail.com>
2026-03-24 00:14:11 +0300

    resellers4

--- app/api/orders/details/route.js
+++ app/api/orders/details/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import connectDB from '../../../../lib/mongodb'
 import Order from '../../../../models/Order'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url)
